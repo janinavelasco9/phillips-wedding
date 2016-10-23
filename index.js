cda3ccdce4a6c512794c13219508367f6d016db0
Janina Velasco <Phillips@Janinas-MacBook-Air.local>
2016-10-23 09:57:47 -0400

    try the rsvp form

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+
+// var router = express.Router();
+// app.use('/rsvp', router);
+// router.post('/rsvp', handleRsvp); // handle the route at yourdomain.com/sayHello
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -23,6 +28,44 @@ app.get('/photographs', function(request, response) {
 
 app.get('/details', function(request, response) {
   response.render('pages/details');
+});
+
+app.get('/rsvp', function(request, response) {
+  response.render('pages/rsvp');
+});
+
+app.post('/rsvp', function(req, res) {
+    // Not the movie transporter!
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: '', // Your email id
+            pass: '' // Your password
+        }
+    });
+
+    var text = 'Hello world from \n\n' + req.body.name;
+
+    var mailOptions = {
+        from: '', // sender address
+        to: '', // list of receivers
+        subject: 'Email Example', // Subject line
+        text: text //, // plaintext body
+        // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+    };
+
+    console.log('test');
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            res.json({yo: 'error'});
+        }else{
+            console.log('Message sent: ' + info.response);
+            res.json({yo: info.response});
+        };
+    });
+    console.log('testtt');
 });
 
 app.listen(app.get('port'), function() {
