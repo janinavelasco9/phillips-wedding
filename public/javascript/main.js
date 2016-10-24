353bb4aedea2702a9a9a2db5a420b331bb8adcab
Janina Velasco <jvelasco0226@gmail.com>
2016-10-24 11:37:38 -0400

    update form styles

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -11,4 +11,31 @@ $( document ).ready(function() {
               $('.nav').slideToggle();
         });
     });
+
+    //material contact form animation
+    $('.form').find('.text-input').each(function() {
+      var targetItem = $(this).parent();
+      if ($(this).val()) {
+        $(targetItem).find('label').css({
+          'top': '10px',
+          'fontSize': '14px'
+        });
+      }
+    })
+    $('.form').find('.text-input').focus(function() {
+      $(this).parent('.input-field').addClass('focus');
+      $(this).parent().find('label').animate({
+        'top': '10px',
+        'fontSize': '14px'
+      }, 300);
+    })
+    $('.form').find('.text-input').blur(function() {
+      if ($(this).val().length == 0) {
+        $(this).parent('.input-field').removeClass('focus');
+        $(this).parent().find('label').animate({
+          'top': '25px',
+          'fontSize': '1.15rem'
+        }, 300);
+      }
+    })
 });
